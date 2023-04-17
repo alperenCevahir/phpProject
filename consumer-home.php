498f3841b698d5ea7e828bf0bfab308945c64ea5
emirn00 <emirnar2701@gmail.com>
2023-04-17 20:51:54 +0300

    Improving consumer land page

---
+++ consumer-home.php
@@ -0,0 +1,35 @@
+<?php ?>
+
+<html>
+    <head>
+        
+        <link rel="stylesheet" href="style.css">
+        <body>
+            
+            <p class="baslik">HOME AUTOMATİON DASHBOARD</p>
+
+            <div class="buyukKutu">
+
+
+            <div class="kutu" >
+                <div class="altKutu"; style="background-color: darkgreen; margin-top : 110px">asjdaskdjas</div>
+            </div>
+
+
+
+            <div class="kutu" >Electronic Lamp
+            <div class="altKutu"; style="background-color: darkgreen; margin-top : 110px">asjdaskdjas</div>
+            </div>
+            <div class="kutu" >
+            <div class="altKutu"; style="background-color: darkgreen; margin-top : 110px">asjdaskdjas</div>
+            </div>
+            <div class="kutu" >
+            <div class="altKutu"; style="background-color: darkgreen; margin-top : 110px">asjdaskdjas</div>
+            </div>
+            </div>
+            
+            
+            
+        </body>
+    </head>
+</html>
