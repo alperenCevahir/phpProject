dcd6ea52c5ab0024f9d09ecae2549658338a6bc2
alperencevahir <alperen.cevahir@hotmail.com>
2023-04-17 16:32:49 +0300

    Bug fix

--- consumer-login.php
+++ consumer-login.php
@@ -1,31 +1,31 @@
+<?php
+    // I check if the form is submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve the form data
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        // I will add authentication here
+
+        // Redirect user to home page after successful login
+        header("Location: home.php");
+        exit;
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Consumer Login</title>
+    <title>Consumer Login</title>
 </head>
 <body>
-	<h2>Consumer Login</h2>
-	<form action="consumer-login.php" method="post">
-		<label for="username">Username:</label>
-		<input type="text" id="username" name="username"><br><br>
-		<label for="password">Password:</label>
-		<input type="password" id="password" name="password"><br><br>
-		<input type="submit" value="Login">
-	</form>
-
-	<?php
-		// I check if the form is submitted
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			// Retrieve the form data
-			$username = $_POST["username"];
-			$password = $_POST["password"];
-
-			// I will add authentication here
-			
-			// Redirect user to home page after successfull login
-			header("Location: home.php");
-			exit;
-		}
-	?>
+    <h2>Consumer Login</h2>
+    <form action="consumer-login.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username"><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Login">
+    </form>
 </body>
 </html>
